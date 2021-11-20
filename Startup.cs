b767e7b0ff522d3f958b8226d53e5e2fc6c9ac8b
Le` Noi <52886036+nointh@users.noreply.github.com>
2021-11-20 14:33:46 +0700

    Add admin layout and its areas

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace ShoesLover
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +49,10 @@ namespace ShoesLover
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
